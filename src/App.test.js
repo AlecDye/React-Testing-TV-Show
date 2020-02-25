b2aaac585525322f02b6541118a33e7aea039457
Alec Dye <alecdye@gmail.com>
2020-02-25 06:52:10 -0800

    adding testings for dropdown and api

--- src/App.test.js
+++ src/App.test.js
@@ -1,18 +1,24 @@
 import React from 'react';
 import { render, getByText, userEvent, debug } from '@testing-library/react';
 import App from './App';
+import { fetchShow } from './api/fetchShow';
+
+// const episodes =
 
 test('Dropdown menu is rendering on app component', () => {
-
+    const mockFetchShow = jest.fn("./api/fetchShow");
     const { getByText } = render(<App />)
 
-    userEvent.change(getByText(/select a season/i));
-    debug()
-    getByText(/season 1/i);
+
+    const getSeason = getByText(/select a season/i);
+    userEvent.change(getSeason);
+    // userEvent.change(getByText(/select a season/i));
+    // debug()
+    // getByText(/season 1/i);
 
 })
 
-// drop down menu (with divs instead of select)
-//? import userEvent from '@tasting-library/react'
-//? userEvent.change(getByText(/select a season/i))
-// getByText(/season 1/i);
+test('Api is being fetched and rendered', async () => {
+
+    mockFetchShow.mockResolveValueOnce(episodes);
+})
--- src/api/fetchShow.js
+++ src/api/fetchShow.js
@@ -6,7 +6,7 @@ const fetchShow = () => {
             "https://api.tvmaze.com/singlesearch/shows?q=stranger-things&embed=episodes"
         )
         .then(res => {
-            console.log(res);
+            console.log("API request", res);
             return res
         })
         .catch(err => {
